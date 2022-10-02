8d0f2120342e4d8dd24f872ff8e2235c3a611ce6
Grafiker <Grafiker@DESKTOP-6HMG5IJ>
2022-10-02 20:12:04 +0300

    HomeWork

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		sayi = -129;
+
+		// char tek karakter tutar, tek tırnaklar arasına yazılır.
+		char karakter = 'A';
+
+		boolean dogruMu = false;
+
+	}
+
+}
