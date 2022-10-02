f67f9503d54c0eac1c8a3ebcaf39f1ba6e9783ff
Grafiker <Grafiker@DESKTOP-6HMG5IJ>
2022-10-02 19:13:46 +0300

    HomeWork

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// case sensitive; Java büyük küçük harf duyarlı bir dildir.
+		// değişken isimleri camelCase yazılır
+
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+
+		// Reusability: Tekrar kullanılabilirlik. Bir kere yaz tekrar tekrar kullan.
+
+	}
+
+}
